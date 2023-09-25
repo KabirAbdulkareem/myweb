617afb089b162b80772d45f22e0248af6c9528e3
Kabir Abdulkareem <kabirabdulkareem@gmail.com>
2023-09-25 23:55:48 +0100

    Styled navbar and footer

--- src/app/dashboard/users/page.jsx
+++ src/app/dashboard/users/page.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import styles from './page.module.css'
 const Users = () => {
   return (
-    <div>Users</div>
+    <div className={styles.container}>Users</div>
   )
 }
 
