0b34cfaf19a23ca38e7a97e1db3c76087d4a88f6
Kabir Abdulkareem <kabirabdulkareem@gmail.com>
2023-09-29 09:44:57 +0100

    Added switch functionality to the Mode toggle component.
    The mode toggle component can now be switch by clicking on it. Changes to the website mode has is yet to be implemented. That would be the next task.

--- src/components/DarkModeToggle/DarkModeToggle.jsx
+++ src/components/DarkModeToggle/DarkModeToggle.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./darkModeToggle.module.css";
 
 const DarkModeToggle = () => {
-  const mode = "light";
+  const [mode, setMode] = useState("dark")
+  const toggleMode = ()=>{
+    setMode((prev) => (prev === 'dark' ? 'bright':'dark'))
+  }
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={toggleMode}>
       <div className={styles.icon}>🌙</div>
       <div className={styles.icon}>🔆</div>
       <div
