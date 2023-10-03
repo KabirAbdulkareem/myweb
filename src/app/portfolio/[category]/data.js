16b57b05196fb8c6bcb6ee57a03949681d916315
Kabir Abdulkareem <kabirabdulkareem@gmail.com>
2023-10-03 16:12:56 +0100

    Created and fetch local data used in the Portfolio Category

---
+++ src/app/portfolio/[category]/data.js
@@ -0,0 +1,71 @@
+export const items = {
+  applications: [
+    {
+      id: 1,
+      title: "Creative Portfolio",
+      desc: "",
+      image:
+        "https://images.pexels.com/photos/3850265/pexels-photo-3850265.jpeg",
+    },
+    {
+      id: 2,
+      title: "Minimal Single Product",
+      desc: "",
+      image:
+        "https://images.pexels.com/photos/3949098/pexels-photo-3949098.jpeg",
+    },
+    {
+      id: 3,
+      title: "Strong Together Charity",
+      desc: "",
+      image:
+        "https://images.pexels.com/photos/3850441/pexels-photo-3850441.jpeg",
+    },
+  ],
+  illustrations: [
+    {
+      id: 1,
+      title: "Creative Portfolio",
+      desc: "",
+      image:
+        "https://images.pexels.com/photos/163036/mario-luigi-yoschi-figures-163036.jpeg",
+    },
+    {
+      id: 2,
+      title: "Minimal Single Product",
+      desc: "",
+      image:
+        "https://images.pexels.com/photos/1289363/pexels-photo-1289363.jpeg",
+    },
+    {
+      id: 3,
+      title: "Strong Together Charity",
+      desc: "",
+      image:
+        "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+    },
+  ],
+  websites: [
+    {
+      id: 1,
+      title: "Creative Portfolio",
+      desc: "",
+      image:
+        "https://images.pexels.com/photos/1029757/pexels-photo-1029757.jpeg",
+    },
+    {
+      id: 2,
+      title: "Minimal Single Product",
+      desc: "",
+      image:
+        "https://images.pexels.com/photos/7310202/pexels-photo-7310202.jpeg",
+    },
+    {
+      id: 3,
+      title: "Strong Together Charity",
+      desc: "",
+      image:
+        "https://images.pexels.com/photos/7309483/pexels-photo-7309483.jpeg",
+    },
+  ],
+};
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -2,12 +2,45 @@ import React from "react";
 import styles from "./page.module.css";
 import Button from "@/components/Button/Button";
 import Image from "next/image";
+import { items } from "./data";
+import { notFound } from "next/navigation";
+
+const getData = (category) => {
+  const data = items[category];
+
+  if (!data) {
+    return notFound();
+  }
+
+  return data;
+};
 
 const Category = ({ params }) => {
+   /* { category: 'illustrations' } */
+  const data = getData(params.category);
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
-      <div className={styles.item}>
+      {data.map((item) => (
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button text="See more" url="#" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image
+              className={styles.img}
+              fill={true}
+              src={item.image}
+              alt=""
+              priority={true}
+            />
+          </div>
+        </div>
+      ))}
+
+      {/* <div className={styles.item}>
         <div className={styles.content}>
           <h1 className={styles.title}>Test</h1>
           <p className={styles.desc}>Desc</p>
@@ -37,23 +70,7 @@ const Category = ({ params }) => {
             alt=""
           />
         </div>
-      </div>
-
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button text="See more" url="#" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-            className={styles.img}
-            fill={true}
-            src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg"
-            alt=""
-          />
-        </div>
-      </div>
+      </div> */}
     </div>
   );
 };
