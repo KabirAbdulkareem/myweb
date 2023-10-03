5e4b98b55744d3686546b3cb4c71c6a534e63eef
Kabir Abdulkareem <kabirabdulkareem@gmail.com>
2023-10-03 09:20:57 +0100

    Dynamically fetched serever side data

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,83 +3,51 @@ import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
-const Blog = () => {
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "no-store",
+  });
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+const Blog = async () => {
+  const data = await getData();
   return (
     <div className={styles.mainContainer}>
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imgContainer}>
-          <Image
-            className={styles.img}
-            width={400}
-            height={250}
-            src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg"
-            alt=""
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
-            officiis nam maiores at provident minima repellat, error amet
-            molestias adipisci cumque nisi accusamus temporibus architecto
-            possimus consequuntur necessitatibus sit harum! Lorem ipsum dolor
-            sit amet consectetur adipisicing elit. Optio officiis nam maiores at
-            provident minima repellat, error amet molestias adipisci cumque nisi
-            accusamus temporibus architecto possimus consequuntur necessitatibus
-            sit harum!
-          </p>
-        </div>
-      </Link>
-
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imgContainer}>
-          <Image
-            className={styles.img}
-            width={400}
-            height={250}
-            src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg"
-            alt=""
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
-            officiis nam maiores at provident minima repellat, error amet
-            molestias adipisci cumque nisi accusamus temporibus architecto
-            possimus consequuntur necessitatibus sit harum! Lorem ipsum dolor
-            sit amet consectetur adipisicing elit. Optio officiis nam maiores at
-            provident minima repellat, error amet molestias adipisci cumque nisi
-            accusamus temporibus architecto possimus consequuntur necessitatibus
-            sit harum!
-          </p>
-        </div>
-      </Link>
-
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imgContainer}>
-          <Image
-            className={styles.img}
-            width={400}
-            height={250}
-            src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg"
-            alt=""
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
-            officiis nam maiores at provident minima repellat, error amet
-            molestias adipisci cumque nisi accusamus temporibus architecto
-            possimus consequuntur necessitatibus sit harum! Lorem ipsum dolor
-            sit amet consectetur adipisicing elit. Optio officiis nam maiores at
-            provident minima repellat, error amet molestias adipisci cumque nisi
-            accusamus temporibus architecto possimus consequuntur necessitatibus
-            sit harum!
-          </p>
-        </div>
-      </Link>
+      {data.map((item) => (
+        <Link href={`/blog/${item.id}`} className={styles.container} key={item.id}>
+          <div className={styles.imgContainer}>
+            <Image
+              className={styles.img}
+              width={400}
+              height={250}
+              src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg"
+              alt=""
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+              officiis nam maiores at provident minima repellat, error amet
+              molestias adipisci cumque nisi accusamus temporibus architecto
+              possimus consequuntur necessitatibus sit harum! Lorem ipsum dolor
+              sit amet consectetur adipisicing elit. Optio officiis nam maiores
+              at provident minima repellat, error amet molestias adipisci cumque
+              nisi accusamus temporibus architecto possimus consequuntur
+              necessitatibus sit harum!
+            </p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
