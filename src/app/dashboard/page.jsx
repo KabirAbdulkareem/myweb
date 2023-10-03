b847d4f6693a8827534e1e4e527ca98e5997e4b3
Kabir Abdulkareem <kabirabdulkareem@gmail.com>
2023-10-03 16:08:15 +0100

    Experimenting with client side data fetching

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import styles from './page.module.css'
+"use client"
+import React, { useEffect, useState } from "react";
+import styles from "./page.module.css";
+import { notFound } from "next/navigation";
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 const Dashboard = () => {
-  return (
-    <div className={styles.container}>Dashboard</div>
-  )
-}
+/*   const [isLoading, setIsLoading] = useState(false)
+  const [data, setData] = useState([])
+  const [err, setErr] = useState(false)
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true)
+      const data = await fetch("https://jsonplaceholder.typicode.com/posts", {
+        cache: "no-store",
+      })
 
-export default Dashboard
+      if(!data.ok){
+        setErr(true)
+        return notFound()
+      }
+
+      const posts = await data.json()
+      setData(posts)
+      setIsLoading(false)
+      console.log(posts)
+      return posts;
+    }
+    getData();
+  },[]); */
+  const { data, error, isLoading } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
+
+  if (error) return "An error has occurred.";
+  if (isLoading) return "Loading...";
+
+  console.log(data)
+  return <div className={styles.container}>Dashboard</div>;
+};
+
+export default Dashboard;
