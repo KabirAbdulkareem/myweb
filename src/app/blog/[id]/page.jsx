bceab0cb3b3f0db2d736b3774851493a86261653
Kabir Abdulkareem <kabirabdulkareem@gmail.com>
2023-10-03 09:22:54 +0100

    Fixed error

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import styles from "./page.module.css";
-import Link from "next/link";
 import Image from "next/image";
+import {notFound} from "next/navigation";
 
-const BlogPost = () => {
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    cache: "no-store",
+  });
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    return notFound()
+  }
+
+  return res.json();
+}
+
+const BlogPost = async ({ params }) => {
+  const data = await getData(params.id);
+
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
           <h1 className={styles.title}>
-            Lorem ipsum dolor sit amet conse ctetur adipi sicing elit adipi sicing.
+            {data.title}
           </h1>
           <p className={styles.desc}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
@@ -42,28 +59,31 @@ const BlogPost = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
           officiis nam maiores at provident minima repellat, error amet
           molestias adipisci cumque nisi accusamus temporibus architecto
-          possimus consequuntur necessitatibus sit harum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
-          officiis nam maiores at provident minima repellat, error amet
-          molestias adipisci cumque nisi accusamus temporibus architecto
-          possimus consequuntur necessitatibus sit harum!
+          possimus consequuntur necessitatibus sit harum! Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Optio officiis nam maiores at
+          provident minima repellat, error amet molestias adipisci cumque nisi
+          accusamus temporibus architecto possimus consequuntur necessitatibus
+          sit harum!
           <br />
           <br />
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
           officiis nam maiores at provident minima repellat, error amet
           molestias adipisci cumque nisi accusamus temporibus architecto
-          possimus consequuntur necessitatibus sit harum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
-          officiis nam maiores at provident minima repellat, error amet
-          molestias adipisci cumque nisi accusamus temporibus architecto
-          possimus consequuntur necessitatibus sit harum!
+          possimus consequuntur necessitatibus sit harum! Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Optio officiis nam maiores at
+          provident minima repellat, error amet molestias adipisci cumque nisi
+          accusamus temporibus architecto possimus consequuntur necessitatibus
+          sit harum!
           <br />
           <br />
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
           officiis nam maiores at provident minima repellat, error amet
           molestias adipisci cumque nisi accusamus temporibus architecto
-          possimus consequuntur necessitatibus sit harum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
-          officiis nam maiores at provident minima repellat, error amet
-          molestias adipisci cumque nisi accusamus temporibus architecto
-          possimus consequuntur necessitatibus sit harum!
+          possimus consequuntur necessitatibus sit harum! Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Optio officiis nam maiores at
+          provident minima repellat, error amet molestias adipisci cumque nisi
+          accusamus temporibus architecto possimus consequuntur necessitatibus
+          sit harum!
         </p>
       </div>
     </div>
